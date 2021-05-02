482902277de8fd8efa5e861ec2b4dacec19492a2
KevinTulakyan <KevinTulakyan@gmail.com>
2021-05-01 18:40:33 -0700

    added basic working ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
-import About from './components/About'
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
+
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [categories] = useState([
+    { name: 'portfolio', description: 'Some of my previous projects.' },
+    { name: 'resume', description: 'Some of the things I am well versed in.' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div className="App">
-      <About />
+    <div>
+      <Nav
+          categories={categories}
+          setCurrentCategory={setCurrentCategory}
+          currentCategory={currentCategory}
+          contactSelected={contactSelected}
+          setContactSelected={setContactSelected}
+        ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Portfolio currentCategory={currentCategory}></Portfolio>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+        contactSelected,
+        setContactSelected
+    } = props;
+
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+
+    return (
+        <header className="flex-row px-1">
+            <h2>
+                <a data-testid="link" href="/">
+                    <span role="img" aria-label="camera">
+                        {" "}
+                    </span>{" "}
+                    Kevin Tulakyan Portfolio
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <a href="#about" onClick={() => setContactSelected(false)}>
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li
+                        className={`mx-1 ${
+                          currentCategory.name === category.name && !contactSelected && `navActive`
+                          }`}
+                        key={category.name}
+                      >
+                            <span
+                                onClick={() => {
+                                    setCurrentCategory(category)
+                                }}
+                            >
+                                {capitalizeFirstLetter(category.name)}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
